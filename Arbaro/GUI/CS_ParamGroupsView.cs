044af5f5fc29f61450a171959275e20c5a6b6666
khazanJM <khazan.jm@gmail.com>
2014-05-01 10:17:35 +0200

    Start GUI

---
+++ Arbaro/GUI/CS_ParamGroupsView.cs
@@ -0,0 +1,108 @@
+﻿using Arbaro2.Arbaro.Params;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arbaro2.Arbaro.GUI
+{
+    public class CS_GroupNode
+    {
+        String groupName;
+        String groupLabel;
+        int groupLevel;
+
+        public CS_GroupNode(String name, String label, int level)
+        {
+
+            groupName = name;
+            groupLabel = label;
+            groupLevel = level;
+        }
+
+        public String getGroupLabel()
+        {
+            return groupLabel;
+        }
+
+        public String getGroupName()
+        {
+            return groupName;
+        }
+
+        public int getGroupLevel()
+        {
+            return groupLevel;
+        }
+    }
+
+    public class CS_ParamGroupsView
+    {
+        private TreeView _tv;
+
+        public CS_ParamGroupsView(TreeView tv)
+        {
+            _tv = tv;
+            CreateNodes();
+        }
+
+        private void CreateNodes()
+        {
+            TreeNode tn;
+
+            CS_GroupNode general = new CS_GroupNode("", "General", CS_AbstractParam.GENERAL);
+            TreeNode tnGeneral = _tv.Nodes.Add(general.getGroupLabel());
+            tnGeneral.Tag = general;
+
+            CS_GroupNode treeShape = new CS_GroupNode("SHAPE", "Tree shape", CS_AbstractParam.GENERAL);
+            tn = tnGeneral.Nodes.Add(treeShape.getGroupLabel());
+            tn.Tag = treeShape;
+
+            CS_GroupNode trunkRadius = new CS_GroupNode("TRUNK", "Trunk radius", CS_AbstractParam.GENERAL);
+            tn = tnGeneral.Nodes.Add(trunkRadius.getGroupLabel());
+            tn.Tag = trunkRadius;
+
+            CS_GroupNode leaves = new CS_GroupNode("LEAVES", "Leaves", CS_AbstractParam.GENERAL);
+            tn = tnGeneral.Nodes.Add(leaves.getGroupLabel());
+            tn.Tag = leaves;
+
+            CS_GroupNode pruning = new CS_GroupNode("PRUNING", "Pruning/Envelope", CS_AbstractParam.GENERAL);
+            tn = tnGeneral.Nodes.Add(pruning.getGroupLabel());
+            tn.Tag = pruning;
+
+            CS_GroupNode quality = new CS_GroupNode("QUALITY", "Quality", CS_AbstractParam.GENERAL);
+            tn = tnGeneral.Nodes.Add(quality.getGroupLabel());
+            tn.Tag = quality;
+
+            for (int i = 0; i < 4; i++)
+            {
+                String lName = "Level " + i;
+                if (i == 0) lName += " (trunk)";
+                CS_GroupNode level = new CS_GroupNode("", lName, i);
+
+                tn = _tv.Nodes.Add(level.getGroupLabel());
+
+                CS_GroupNode lentaper = new CS_GroupNode("LENTAPER", "Length and taper", i);
+                TreeNode tnn = tn.Nodes.Add(lentaper.getGroupLabel());
+                tnn.Tag = lentaper;
+
+                CS_GroupNode curvature = new CS_GroupNode("CURVATURE", "Curvature", i);
+                tnn = tn.Nodes.Add(curvature.getGroupLabel());
+                tnn.Tag = curvature;
+
+                CS_GroupNode splitting = new CS_GroupNode("SPLITTING", "Splitting", i);
+                tnn = tn.Nodes.Add(splitting.getGroupLabel());
+                tnn.Tag = splitting;
+
+                CS_GroupNode branching = new CS_GroupNode("BRANCHING", "Branching", i);
+                tnn = tn.Nodes.Add(branching.getGroupLabel());
+                tnn.Tag = branching;
+            }
+
+
+            //setSelectionPath(new TreePath(firstGroup.getPath()));
+        }
+    }
+}
--- ArbaroMainForm.Designer.cs
+++ ArbaroMainForm.Designer.cs
@@ -28,20 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.mainMenuStrip.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Arbaro
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(799, 24);
+            this.mainMenuStrip.TabIndex = 0;
+            this.mainMenuStrip.Text = "mainMenuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Alt) 
+            | System.Windows.Forms.Keys.S)));
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.saveAsToolStripMenuItem.Text = "Save as...";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.treeView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 426);
+            this.panel1.TabIndex = 1;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(198, 253);
+            this.treeView1.TabIndex = 0;
+            // 
+            // ArbaroMainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(799, 450);
-            this.Name = "Arbaro";
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.mainMenuStrip);
+            this.MainMenuStrip = this.mainMenuStrip;
+            this.Name = "ArbaroMainForm";
             this.Text = "Arbaro C# V0.1";
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TreeView treeView1;
     }
 }
 
