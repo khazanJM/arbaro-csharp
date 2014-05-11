8f374421669370d0c1c0ab906ce8b1a382dfcd0e
khazanJM <khazan.jm@gmail.com>
2014-05-11 09:15:09 +0200

    pan zoom and rotate should work as intended

--- DX_Engine/DXOrbitControls.cs
+++ DX_Engine/DXOrbitControls.cs
@@ -22,13 +22,12 @@ using System.Windows.Forms;
 namespace Arbaro2.DX_Engine
 {
     public class DXOrbitControls
-    {       
+    {
         private float _MouseX, _MouseY;
         private bool _MouseDown = false;
-        private Control _Ctrl;
-        private float _rx, _ry; 
+        private Control _Ctrl;    
         private DXCamera _camera;
-   
+
         // ctrl is the control hooked for events (mouse & keyboard)
         public DXOrbitControls(DXCamera camera, Control ctrl)
         {
@@ -39,28 +38,26 @@ namespace Arbaro2.DX_Engine
             ctrl.MouseUp += ctrl_MouseUp;
             ctrl.MouseWheel += ctrl_MouseWheel;
             ctrl.KeyDown += ctrl_KeyDown;
-         
+
             _MouseX = 0; _MouseY = 0;
-            _rx = 0; _ry = 0;
         }
 
         public void Reset()
         {
             _MouseX = 0; _MouseY = 0;
-            _rx = 0; _ry = 0;
         }
- 
+
 
         void ctrl_KeyDown(object sender, KeyEventArgs e)
-        {                     
+        {
         }
 
 
         void ctrl_MouseWheel(object sender, MouseEventArgs e)
         {
             float zoom = e.Delta;
-            Vector3 rail = _camera.Target - _camera.Position;            
-            UpdateCamera(0, 0, Math.Sign(zoom)*0.1f*rail, Vector3.Zero);
+            Vector3 rail = _camera.Target - _camera.Position;
+            UpdateCamera(0, 0, Math.Sign(zoom) * 0.1f * rail, Vector3.Zero);
         }
 
         void ctrl_MouseUp(object sender, MouseEventArgs e)
@@ -74,28 +71,30 @@ namespace Arbaro2.DX_Engine
         {
             if (!_MouseDown) return;
 
-            float DX = (e.X - _MouseX)/_Ctrl.ClientSize.Width;
-            float DY = (e.Y - _MouseY)/_Ctrl.ClientSize.Height;
+            float DX = (e.X - _MouseX) / _Ctrl.ClientSize.Width;
+            float DY = (e.Y - _MouseY) / _Ctrl.ClientSize.Height;
             _MouseX = e.X;
             _MouseY = e.Y;
 
-            if (e.Button == MouseButtons.Left) {
+            if (e.Button == MouseButtons.Left)
+            {
                 // Rotate
                 DX = (float)Math.PI * DX;
                 DY = (float)Math.PI * DY;               
-                _rx += DX;
-                _ry -= DY;
-                UpdateCamera(_rx, _ry, Vector3.Zero, Vector3.Zero);
+                UpdateCamera(-DX, DY, Vector3.Zero, Vector3.Zero);
             }
             else if (e.Button == MouseButtons.Right)
             {
                 // Pan
                 //  The camera and the target are translated with the same
-                //  translation. The translation vector is in the tangent plane
-                //  to the orbit (translation along left & up vector)
-
-                Vector3 translation = 100*(-DX*_camera.Left+DY*_camera.Up);
-                UpdateCamera(_rx, _ry, translation, translation);
+                //  translation. 
+                Vector3 l = Vector3.Cross((_camera.Target - _camera.Position), new Vector3(0, 1, 0));
+                l.Normalize();
+                Vector3 u = Vector3.Cross((_camera.Target - _camera.Position), l);
+                u.Normalize();
+               
+                Vector3 translation = 10 * (DX * l + -DY * u);
+                UpdateCamera(0, 0, translation, translation);
             }
         }
 
@@ -120,13 +119,32 @@ namespace Arbaro2.DX_Engine
             Vector3 position = p; position.Z -= distance;
 
             _camera.Position = position;
-            _camera.Target = p;            
+            _camera.Target = p;
         }
 
         void UpdateCamera(float dYaw, float dPitch, Vector3 dPosition, Vector3 dTarget)
         {
             _camera.Position += dPosition;
             _camera.Target += dTarget;
+
+            if (dYaw != 0 || dPitch != 0)
+            {
+                Vector3 P = _camera.Position - _camera.Target;
+                float r = P.Length();
+                P /= r;
+                float a = (float)Math.Atan2(P.Z, P.X);
+                float b = (float)Math.Asin(P.Y);
+
+                a += dYaw;
+                b += dPitch;
+
+                P.Y = (float)Math.Sin(b);
+                P.X = (float)(Math.Cos(b) * Math.Cos(a));
+                P.Z = (float)(Math.Cos(b) * Math.Sin(a));
+                P *= r;
+                P += _camera.Target;
+                _camera.Position = P;
+            }
         }
     }
 }
